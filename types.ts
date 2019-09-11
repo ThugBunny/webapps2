1a54f5b00366830d7892fea1f0f62757f0a18644
ThugBunny <52386312+ThugBunny@users.noreply.github.com>
2019-09-11 03:30:49 -0600

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,10 @@
+type CardinalDirection =
+"North"
+| "East"
+| "South"
+| "West";
+function move(distance: number, direction: CardinalDirection) {
+// ...
+}
+move(1,"North"); // Okay
+move(1,"Nurth"); // Error!
